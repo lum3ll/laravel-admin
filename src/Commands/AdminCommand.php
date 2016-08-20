7909997c643928b51f57ba23cf5d7524600c69de
jordanbardsley7 <jordanbardsley7@yahoo.com>
2016-08-19 22:40:47 -0400

    extending GeneratorCommand

--- src/Commands/AdminCommand.php
+++ src/Commands/AdminCommand.php
@@ -2,13 +2,32 @@
 
 namespace LaravelAdmin\Commands;
 
-use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
 use Illuminate\Console\AppNamespaceDetectorTrait;
 
-class AdminCommand extends Command
+class AdminCommand extends GeneratorCommand
 {
     use AppNamespaceDetectorTrait;
 
+    /**
+     * The command name.
+     *
+     * @var string
+     */
+    protected $name = 'admin';
+
+    /**
+     * The command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the laravel admin backend.';
+
+    /**
+     * Fire the admin command.
+     *
+     * @return void
+     */
     public function fire()
     {
         
