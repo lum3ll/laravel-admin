bda4507b051c70f33f2085588ac0e5153415a85c
jordanbardsley7 <jordanbardsley7@yahoo.com>
2016-08-19 21:41:04 -0400

    added admincommand + admincontroller stub

---
+++ src/Commands/AdminCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LaravelAdmin\Commands\AdminCommand;
+
+use Illuminate\Console\Command;
+use Illuminate\Console\AppNamespaceDetectorTrait;
+
+class AdminCommand extends Command
+{
+    use AppNamespaceDetectorTrait;
+
+    public function fire()
+    {
+
+    }
+}
