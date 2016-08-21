a17e79151a955ee145364872d3077ea40517e5d6
jordanbardsley7 <jordanbardsley7@yahoo.com>
2016-08-21 02:15:53 -0400

    controllers + updated stubs

--- src/Commands/AdminCommand.php
+++ src/Commands/AdminCommand.php
@@ -31,7 +31,8 @@ class AdminCommand extends Command
     protected $directories = [
         '/resources/views/admin',
         '/resources/views/templates',
-        '/resources/views/admin/auth'
+        '/resources/views/admin/auth',
+        '/app/Http/Controllers/Admin'
     ];
 
     /**
@@ -44,7 +45,8 @@ class AdminCommand extends Command
         '/resources/views/admin/auth/login.blade.php' => 'views/auth/login.stub',
         '/resources/views/admin/dashboard.blade.php' => 'views/dashboard.stub',
         '/database/migrations/created_admin_users_table.php' => 'migrations/create_admin_users_table.stub',
-        '/app/Http/Controllers/AdminController.php' => 'controllers/AdminAuthController.stub'
+        '/app/Http/Controllers/Admin/AuthController.php' => 'controllers/admin/AuthController.stub',
+        '/app/Http/Controllers/Admin/DashboardController.php' => 'controllers/admin/DashboardController.stub'
     ];
 
     /**
